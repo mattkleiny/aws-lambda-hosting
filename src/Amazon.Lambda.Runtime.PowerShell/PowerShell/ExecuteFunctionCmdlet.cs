0c82d29d473f2049d74313126faf1d11c95a9a15
Matt Kleinschafer <xeusalmighty@gmail.com>
2018-05-26 12:02:37 +1000

    Add a project for managed PowerShell execution of functions.

---
+++ src/Amazon.Lambda.Runtime.PowerShell/PowerShell/ExecuteFunctionCmdlet.cs
@@ -0,0 +1,19 @@
+﻿using System.Management.Automation;
+
+namespace Amazon.Lambda.PowerShell
+{
+  /// <summary>A <see cref="PSCmdlet"/> for invoking a lambda function from PowerShell.</summary>
+  [Cmdlet(VerbsLifecycle.Invoke, "Function")]
+  public sealed class InvokeFunctionCmdlet : PSCmdlet
+  {
+    [Parameter(HelpMessage = "The name of hte function to execute", Mandatory = true)]
+    public string FunctionName { get; set; }
+
+    protected override void EndProcessing()
+    {
+      // TODO: think about bootstrapping here
+      
+      base.EndProcessing();
+    }
+  }
+}
