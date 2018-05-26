811292146abe00ea0099e970f2dc390d4ae0c82e
Matt Kleinschafer <xeusalmighty@gmail.com>
2018-05-26 14:50:31 +1000

    Use Guid instead of auto-incrementing key.

--- src/Amazon.Lambda.Runtime.Example.ServiceStack/Model/BlogPost.cs
+++ src/Amazon.Lambda.Runtime.Example.ServiceStack/Model/BlogPost.cs
@@ -1,12 +1,13 @@
-﻿using ServiceStack.DataAnnotations;
+﻿using System;
+using ServiceStack.DataAnnotations;
 
 namespace Amazon.Lambda.Runtime.Example.ServiceStack.Model
 {
   [Alias("BlogPosts")]
   public sealed class BlogPost
   {
-    [AutoIncrement]
-    public long Id { get; set; }
+    [PrimaryKey]
+    public Guid Id { get; set; } = Guid.NewGuid();
 
     public string Title { get; set; }
     public string Slug  { get; set; }
