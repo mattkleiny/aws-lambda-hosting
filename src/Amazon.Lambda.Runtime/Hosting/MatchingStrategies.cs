dc53e238a834d86a30fdc8ca596d6b424efe9ae4
Matt Kleinschafer <matt.kleinschafer@xero.com>
2018-07-24 10:15:38 +1000

    Add default strategies for matching functions by ARN.

--- src/Amazon.Lambda.Runtime/Hosting/MatchingStrategies.cs
+++ src/Amazon.Lambda.Runtime/Hosting/MatchingStrategies.cs
@@ -10,18 +10,18 @@ namespace Amazon.Lambda.Hosting
   public static class MatchingStrategies
   {
     public static MatchingStrategy MatchByName(StringComparison comparison = StringComparison.OrdinalIgnoreCase)
-    {
-      return (registration, input, context) => context.FunctionName.Equals(registration.FunctionName, comparison);
-    }
+      => (registration, input, context) => context.FunctionName.Equals(registration.FunctionName, comparison);
 
     public static MatchingStrategy MatchByNamePrefix(StringComparison comparison = StringComparison.OrdinalIgnoreCase)
-    {
-      return (registration, input, context) => context.FunctionName.StartsWith(registration.FunctionName, comparison);
-    }
+      => (registration, input, context) => context.FunctionName.StartsWith(registration.FunctionName, comparison);
 
     public static MatchingStrategy MatchByNameSuffix(StringComparison comparison = StringComparison.OrdinalIgnoreCase)
-    {
-      return (registration, input, context) => context.FunctionName.EndsWith(registration.FunctionName, comparison);
-    }
+      => (registration, input, context) => context.FunctionName.EndsWith(registration.FunctionName, comparison);
+
+    public static MatchingStrategy MatchByArn(StringComparison comparison = StringComparison.OrdinalIgnoreCase)
+      => (registration, input, context) => context.InvokedFunctionArn.Equals(registration.FunctionName, comparison);
+
+    public static MatchingStrategy MatchByArnSuffix(StringComparison comparison = StringComparison.OrdinalIgnoreCase)
+      => (registration, input, context) => context.InvokedFunctionArn.EndsWith(registration.FunctionName, comparison);
   }
 }
