8ee5afd939027e5a5714c54039b7c94af86b7e58
Matt Kleinschafer <matt.kleinschafer@xero.com>
2018-07-26 14:08:37 +1000

    Make the LocalLambdaContext mutable and align the local/test contexts in similarity.

--- src/Amazon.Lambda.Runtime/Hosting/LocalLambdaContext.cs
+++ src/Amazon.Lambda.Runtime/Hosting/LocalLambdaContext.cs
@@ -10,20 +10,20 @@ namespace Amazon.Lambda.Hosting
     public LocalLambdaContext(string functionName)
     {
       Check.NotNullOrEmpty(functionName, nameof(functionName));
-      
+
       FunctionName = functionName;
     }
 
-    public string           AwsRequestId       { get; } = Guid.NewGuid().ToString();
-    public IClientContext   ClientContext      { get; } = null;
-    public string           FunctionName       { get; }
-    public string           FunctionVersion    { get; } = "Unknown";
-    public ICognitoIdentity Identity           { get; } = null;
-    public string           InvokedFunctionArn { get; } = string.Empty;
-    public ILambdaLogger    Logger             { get; } = ConsoleLambdaLogger.Instance;
-    public string           LogGroupName       { get; } = string.Empty;
-    public string           LogStreamName      { get; } = string.Empty;
-    public int              MemoryLimitInMB    { get; } = int.MaxValue;
-    public TimeSpan         RemainingTime      { get; } = TimeSpan.MaxValue;
+    public string           AwsRequestId       { get; set; } = Guid.NewGuid().ToString();
+    public IClientContext   ClientContext      { get; set; } = null;
+    public string           FunctionName       { get; set; }
+    public string           FunctionVersion    { get; set; } = "$LATEST";
+    public ICognitoIdentity Identity           { get; set; } = null;
+    public string           InvokedFunctionArn { get; set; } = string.Empty;
+    public ILambdaLogger    Logger             { get; set; } = ConsoleLambdaLogger.Instance;
+    public string           LogGroupName       { get; set; } = string.Empty;
+    public string           LogStreamName      { get; set; } = string.Empty;
+    public int              MemoryLimitInMB    { get; set; } = 3000;                      // maximum memory limit in AWS
+    public TimeSpan         RemainingTime      { get; set; } = TimeSpan.FromSeconds(300); // maximum execution time for a lambda in AWS
   }
 }
--- src/Amazon.Lambda.Runtime/Testing/TestLambdaContext.cs
+++ src/Amazon.Lambda.Runtime/Testing/TestLambdaContext.cs
@@ -7,37 +7,16 @@ namespace Amazon.Lambda.Testing
   /// <summary>A <see cref="ILambdaContext"/> for testing.</summary>
   public sealed class TestLambdaContext : ILambdaContext
   {
-    public TestLambdaContext()
-    {
-    }
-
-    public TestLambdaContext(ILambdaContext other)
-    {
-      Check.NotNull(other, nameof(other));
-
-      AwsRequestId       = other.AwsRequestId;
-      ClientContext      = other.ClientContext;
-      FunctionName       = other.FunctionName;
-      FunctionVersion    = other.FunctionVersion;
-      Identity           = other.Identity;
-      InvokedFunctionArn = other.InvokedFunctionArn;
-      Logger             = other.Logger;
-      LogGroupName       = other.LogGroupName;
-      LogStreamName      = other.LogStreamName;
-      MemoryLimitInMB    = other.MemoryLimitInMB;
-      RemainingTime      = other.RemainingTime;
-    }
-    
     public string           AwsRequestId       { get; set; } = Guid.NewGuid().ToString();
-    public IClientContext   ClientContext      { get; set; }
-    public string           FunctionName       { get; set; }
-    public string           FunctionVersion    { get; set; } = "Unknown";
-    public ICognitoIdentity Identity           { get; set; }
+    public IClientContext   ClientContext      { get; set; } = null;
+    public string           FunctionName       { get; set; } = string.Empty;
+    public string           FunctionVersion    { get; set; } = "$LATEST";
+    public ICognitoIdentity Identity           { get; set; } = null;
     public string           InvokedFunctionArn { get; set; } = string.Empty;
     public ILambdaLogger    Logger             { get; set; } = NullLambdaLogger.Instance;
     public string           LogGroupName       { get; set; } = string.Empty;
     public string           LogStreamName      { get; set; } = string.Empty;
-    public int              MemoryLimitInMB    { get; set; } = int.MaxValue;
+    public int              MemoryLimitInMB    { get; set; } = 3000;                      // maximum memory limit in AWS
     public TimeSpan         RemainingTime      { get; set; } = TimeSpan.FromSeconds(300); // maximum execution time for a lambda in AWS
   }
 }
